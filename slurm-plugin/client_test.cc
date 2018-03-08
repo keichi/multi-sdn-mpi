28e4a9e2aea1eec329b3add4e5b5830ffd9dbf49
Keichi Takahashi <keichi.t@me.com>
2018-03-08 15:42:02 +0900

    Add some tests for gRPC stub

--- slurm-plugin/client_test.cc
+++ slurm-plugin/client_test.cc
@@ -8,18 +8,38 @@ int main(int argc, char **argv)
     const auto chan = grpc::CreateChannel("localhost:50051", cred);
     const auto stub = SDNMPI::NewStub(chan);
 
-    Empty request;
-    ListJobResponse response;
+    // Empty request;
+    // ListJobResponse response;
+    // grpc::ClientContext context;
+
+    // auto status = stub->ListJob(&context, request, &response);
+
+    // if (status.ok()) {
+    //     std::cout << "ok!" << std::endl;
+
+    //     for (const auto& job : response.jobs()) {
+    //         std::cout << job.id() << std::endl;
+    //     }
+    // } else {
+    //     std::cout << "ng!" << std::endl;
+    // }
+
+    CreateJobRequest request;
+    Empty response;
     grpc::ClientContext context;
 
-    auto status = stub->ListJob(&context, request, &response);
+    request.mutable_job()->set_id(2);
+    request.mutable_job()->set_name("foo");
+    request.mutable_job()->set_uid(1000);
+    request.mutable_job()->set_gid(1000);
+    request.mutable_job()->set_comm_pattern("test");
+    request.mutable_job()->set_n_tasks(10);
+    request.mutable_job()->set_state(JOB_RUNNING);
+
+    auto status = stub->CreateJob(&context, request, &response);
 
     if (status.ok()) {
         std::cout << "ok!" << std::endl;
-
-        for (const auto& job : response.jobs()) {
-            std::cout << job.id() << std::endl;
-        }
     } else {
         std::cout << "ng!" << std::endl;
     }
